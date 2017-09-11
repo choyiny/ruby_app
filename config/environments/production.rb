1196c3484b9a1f596596944e96b76553ab873c58
Cho Yin Yong <choyin.yong@mail.utoronto.ca>
2017-09-11 12:43:26 -0400

    starting chapter 3 and gitignore for secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-FirstApp::Application.configure do
+RubyApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
