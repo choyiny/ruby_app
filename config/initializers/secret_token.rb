5df371ace7a3393f10d16f7512fb0d517ed6ee40
Cho Yin Yong <choyin.yong@mail.utoronto.ca>
2017-09-11 12:24:19 -0400

    2.2 - User resources
    https://rails-4-0.railstutorial.org/book/demo_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '8e107973acca1a6513525b5a4bfc7b817324e500fda67635380c9d595ef7d72b811164792e9db725a30f048aa5c1f26c634239a41800210ef4037250b51b0a5a'
