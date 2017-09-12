b8f3625040656e78a883d2b1d7d2014050c1aed4
Cho Yin Yong <choyin.yong@mail.utoronto.ca>
2017-09-12 09:46:58 -0400

    starting password validation features (chapter 6)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ActiveRecord::Base
-  # validation: name and email cannot be blank
-  validates(:name, presence: true)
-  validates(:email, presence: true)
+
+  # before saving the user object to the database, turn email to all lowercase
+  before_save { self.email = email.downcase }
+
+  # validation: name cannot be blank, and more than 50 characters
+  validates(:name, presence: true, length: { maximum: 50})
+
+  # define regex for email (defined as a constant, starting with upper case letters)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # validation: email cannot be blank, and must follow email format
+  validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: true})
+
+  # this creates password, password_confirmation, password_digest attributes, and authenticate method.
+  has_secure_password
+
+  # pwd should be at least 6 chars
+  validates :password, length: { minimum: 6 }
+
 end
