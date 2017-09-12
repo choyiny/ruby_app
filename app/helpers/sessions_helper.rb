394759d38809c6a42b65797586b57d0f58c8fc3e
Cho Yin Yong <choyin.yong@mail.utoronto.ca>
2017-09-12 14:33:02 -0400

    fix syntax error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,7 @@ module SessionsHelper
 
   def sign_out(user)
     current_user.update_attribute(:remember_token,
-                                  User.digest(User.new_remember_token)
+                                  User.digest(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
   end
