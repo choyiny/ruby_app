9b6da462b964cb78a0173e571759e1936bcaab8a
Cho Yin Yong <choyin.yong@mail.utoronto.ca>
2017-09-13 19:21:04 -0400

    creating microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,8 +5,26 @@ class MicropostsController < ApplicationController
   end
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    # if micropost is valid
+    if @micropost.save
+      # make success message
+      flash[:success] = "Micropost created!"
+      # redirect to the home page
+      redirect_to root_url
+    else
+      @feed_items = []
+      # redirect to home without changing anything
+      render 'static_pages/home'
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def micropost_params
+      params.require(:micropost).permit(:content)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,11 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # ? stops sql injection
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def create_remember_token
